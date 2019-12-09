8e15c58fd727210e1db96622bd4307832600d32d
Chris <chrisk844@gmail.com>
2019-12-08 17:06:02 -0700

    Working on parsing http get request

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 
@@ -26,7 +27,7 @@ public class Main extends Application {
         Button Chair = new Button("Chair");
         Chair.setOnAction(e -> {
             try {
-                URL url2 = new URL("http://192.168.0.20/api/1dum7N67DnTLeFjxGcZrQHp38RmniQE86jjPR06R/lights/1/");
+                URL url2 = new URL("http://192.168.0.20/api/1dum7N67DnTLeFjxGcZrQHp38RmniQE86jjPR06R/lights/");
                 HttpURLConnection connection2 = (HttpURLConnection) url2.openConnection();
                 connection2.setRequestMethod("GET");
                 StringBuilder content;
@@ -48,7 +49,7 @@ public class Main extends Application {
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("PUT");
 
-                String putData = "{\"on\":false}";
+                String putData = "{\"on\":true}";
 
                 byte[] putDataBytes = putData.getBytes(StandardCharsets.UTF_8);
                 connection.setDoOutput(true);
@@ -121,7 +122,37 @@ public class Main extends Application {
     }
 
     public Hashtable makeHash(String string){
+        Hashtable<String, Integer> ht = new Hashtable<String, Integer>();
+        String k1;
+        String v1;
+        char[] chars = string.toCharArray();
+        hashHelpRecursive(string, chars);
 
         return new Hashtable();
+    }
+
+    public void hashHelpRecursive(String string, char[] chars){
+        String[] s2 = string.split("\\},");
+//        for (int i = 0; i<chars.length; i++) {
+//            if(chars[i] == '{'){
+//                string.split("\\{");
+//            }
+//        }
+        for (String item:s2) {
+
+            String[] item2 = item.split("(:)",2);
+            for(String item3:item2){
+                if(item3.startsWith("{")){
+                    System.out.println(item3);
+                }
+                else {
+//                    System.out.print("    " + item3);
+                    String[] item4 = item3.split("(:)",2);
+                    for (String item5:item4) {
+                        System.out.println("    " + item5);
+                    }
+                }
+            }
+        }
     }
 }
